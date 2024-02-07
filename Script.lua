e3ea06caae6f6688684ffde0cd434e34ad0a8f04
Miliudqq <159026791+Miliudqq@users.noreply.github.com>
2024-02-07 19:55:58 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,14 +1,1 @@
-local KeySystemUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/ui/xrer_mstudio45.lua"))()
-KeySystemUI.New({
-    ApplicationName = "ChillX", -- Your Key System Application Name
-    Name = "ChillX", -- Your Script name
-    Info = "Better script for this game", -- Info text in the GUI, keep empty for default text.
-    DiscordInvite = "" -- Optional.
-})
-repeat task.wait() until KeySystemUI.Finished() or KeySystemUI.Closed
-if KeySystemUI.Finished() and KeySystemUI.Closed == false then
-    print("Key verified, can load script")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vwSaraa/LunarHub/main/mm2"))()
-else
-    print("Player closed the GUI.")
-end
+
